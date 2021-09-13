3ac1e85c09cdeca4d69068977e418def2a6e8ba6
Fifer2 <KuryakovDenis@yandex.ru>
2021-09-13 21:57:04 +0700

    feat: Подключение модулей в index.js

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const versionModule = require('../cli/version');
+const helpModule = require('../cli/help');
+const generateModule = require('../cli/generate');
+
+const Cli = {
+  [versionModule.name]: versionModule,
+  [helpModule.name]: helpModule,
+  [generateModule.name]: generateModule
+};
+
+module.exports = {
+  Cli
+};
