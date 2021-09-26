8651364b9ab388589ce83b16792aa7793cdcd2d8
Fifer2 <KuryakovDenis@yandex.ru>
2021-09-26 22:46:38 +0700

    feat: Создание и подключение модуля сервера

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -3,11 +3,13 @@
 const versionModule = require(`../cli/version`);
 const helpModule = require(`../cli/help`);
 const generateModule = require(`../cli/generate`);
+const serverModule = require(`../cli/server`);
 
 const Cli = {
   [versionModule.name]: versionModule,
   [helpModule.name]: helpModule,
-  [generateModule.name]: generateModule
+  [generateModule.name]: generateModule,
+  [serverModule.name]: serverModule
 };
 
 module.exports = {
---
+++ src/service/cli/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const chalk = require(`chalk`);
+const http = require(`http`);
+
+module.exports = {
+  name: `--server`,
+  run(args) {
+
+  }
+};
