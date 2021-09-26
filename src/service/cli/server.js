e981218dae5402ba9f950dcff02fb48e9a77f9b6
Fifer2 <KuryakovDenis@yandex.ru>
2021-09-26 23:20:42 +0700

    feat: Написание функции sendResponse

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const chalk = require(`chalk`);
-const http = require(`http`);
 const fs = require(`fs`).promises;
-const {DEFAULT_PORT, FILE_NAME, HttpCode} = require(`../../const`);
+const http = require(`http`);
+const {DEFAULT_PORT, FILE_NAME, HttpCode, NOT_FOUND_TEXT} = require(`../../const`);
 
 module.exports = {
   name: `--server`,
@@ -11,7 +11,22 @@ module.exports = {
     const [enteredPort] = args;
     const port = Number.parseInt(enteredPort, 10) || DEFAULT_PORT;
 
-    const sendResponse = () => {};
+    const sendResponse = (res, statusCode, message) => {
+      const template = `
+        <!Doctype html>
+          <html lang="ru">
+          <head>
+            <title>With love from Node</title>
+          </head>
+          <body>${message}</body>
+        </html>`.trim();
+
+      http.writeHead(statusCode, {
+        'Content-Type': `text/html; charset=UTF-8`,
+      });
+
+      http.end(template);
+    };
 
     const onClientConnect = async (req, res) => {
       switch (req.url) {
@@ -20,15 +35,15 @@ module.exports = {
             const fileContent = await fs.readFile(FILE_NAME);
             const mocks = JSON.parse(fileContent);
             const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-            sendResponse();
+            sendResponse(res, HttpCode.SUCCESS, `<ul>${message}</ul>`);
           } catch (error) {
             console.error(chalk.red(error));
-            sendResponse();
+            sendResponse(res, HttpCode.NOT_FOUND, NOT_FOUND_TEXT);
           }
 
           break;
         default:
-          sendResponse();
+          sendResponse(res, HttpCode.NOT_FOUND, NOT_FOUND_TEXT);
       }
     };
 
