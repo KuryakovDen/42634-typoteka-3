8625b7030d5305c2f4fe734796dfa09fdf0b4e4f
Fifer2 <KuryakovDenis@yandex.ru>
2021-09-26 22:58:15 +0700

    feat: Создание сервера

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -2,10 +2,21 @@
 
 const chalk = require(`chalk`);
 const http = require(`http`);
+const {DEFAULT_PORT} = require(`../../const`);
 
 module.exports = {
   name: `--server`,
   run(args) {
+    const [enteredPort] = args;
+    const port = Number.parseInt(enteredPort, 10) || DEFAULT_PORT;
 
+    const onClientConnect = () => {};
+
+    const onSendResponse = () => {};
+
+    http.createServer(onClientConnect())
+      .listen(port)
+      .on(`listening`, () => console.info(chalk.green(`Ожидаю подключений на порту ${port}`)))
+      .on(`error`, ({message}) => console.error(chalk.red(`Ошибка при создании сервера: ${message}`)))
   }
 };
