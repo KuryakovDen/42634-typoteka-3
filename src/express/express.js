1293485348b248b1752cd061dfc519c6e70f7051
Fifer2 <KuryakovDenis@yandex.ru>
2021-09-27 17:23:37 +0700

    feat: Создание и подключение my роутов

--- src/express/express.js
+++ src/express/express.js
@@ -3,8 +3,10 @@
 const express = require(`express`);
 const {DEFAULT_SERVER_PORT} = require(`../const`);
 const mainRoutes = require(`./routes/main-routes`);
+const myRoutes = require(`./routes/my-routes`);
 
 const app = express();
 app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
 
 app.listen(DEFAULT_SERVER_PORT);
