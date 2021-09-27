511a778adb3d964c3e7636689af8f61ddf0be297
Fifer2 <KuryakovDenis@yandex.ru>
2021-09-27 17:18:20 +0700

    feat: Создание и подключение главных роутов

--- src/express/express.js
+++ src/express/express.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const {DEFAULT_SERVER_PORT} = require(`../const`);
 const express = require(`express`);
+const {DEFAULT_SERVER_PORT} = require(`../const`);
+const mainRoutes = require(`./routes/main-routes`);
 
 const app = express();
+app.use(`/`, mainRoutes);
 
 app.listen(DEFAULT_SERVER_PORT);
