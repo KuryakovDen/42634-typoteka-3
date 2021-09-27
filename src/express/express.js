f490cf7ed78cccfc08e7092dc946482181f02fbe
Fifer2 <KuryakovDenis@yandex.ru>
2021-09-27 17:31:30 +0700

    feat: Создание и подключение articles роутов

--- src/express/express.js
+++ src/express/express.js
@@ -4,9 +4,11 @@ const express = require(`express`);
 const {DEFAULT_SERVER_PORT} = require(`../const`);
 const mainRoutes = require(`./routes/main-routes`);
 const myRoutes = require(`./routes/my-routes`);
+const articlesRoutes = require(`./routes/articles-routes`);
 
 const app = express();
 app.use(`/`, mainRoutes);
 app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
 
 app.listen(DEFAULT_SERVER_PORT);
