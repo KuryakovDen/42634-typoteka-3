f4f26d136ef35310f5ad5829b393683e3ae30692
Fifer2 <KuryakovDenis@yandex.ru>
2021-10-03 21:58:40 +0700

    feat: Добавление шаблона post.pug

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -4,7 +4,7 @@ const {Router} = require(`express`);
 const articlesRouter = new Router();
 
 articlesRouter.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
-articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
+articlesRouter.get(`/add`, (req, res) => res.render(`post`));
 articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
 articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id`));
 
