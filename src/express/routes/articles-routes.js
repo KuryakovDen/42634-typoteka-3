58a8a47f3e043793593aaeb6d42f634bf1964740
Fifer2 <KuryakovDenis@yandex.ru>
2021-10-04 19:59:55 +0700

    refactor: Рефакторинг

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -3,9 +3,9 @@
 const {Router} = require(`express`);
 const articlesRouter = new Router();
 
-articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
-articlesRouter.get(`/add`, (req, res) => res.render(`post`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`post`));
-articlesRouter.get(`/:id`, (req, res) => res.render(`post-detail`));
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
+articlesRouter.get(`/add`, (req, res) => res.render(`articles/post`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`articles/post`));
+articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post-detail`));
 
 module.exports = articlesRouter;
