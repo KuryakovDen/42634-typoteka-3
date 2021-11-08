c56049d03d1bcc043a35ed4a7420d1224c40d2f3
Fifer2 <KuryakovDenis@yandex.ru>
2021-11-08 19:26:23 +0700

    fix: Исправление тестов для категорий

--- src/service/data-service/category.js
+++ src/service/data-service/category.js
@@ -8,6 +8,8 @@ class CategoryService {
   findAll() {
     const categories = this._articles.reduce((acc, article) => {
       article.category.forEach((category) => acc.add(category));
+
+      return acc;
     }, new Set());
 
     return [...categories];
