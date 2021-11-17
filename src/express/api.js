67ad38bc27e71589d6e0f989f19585447a3e1593
Fifer2 <KuryakovDenis@yandex.ru>
2021-11-17 21:13:12 +0700

    feat: Создание методов ля получения и отправки данных

--- src/express/api.js
+++ src/express/api.js
@@ -9,4 +9,33 @@ class API {
       timeout
     });
   }
+
+  async _load(url, options) {
+    const response = await this._http.request({url, ...options});
+
+    return response.data;
+  }
+
+  async createOffer(data) {
+    return this._load(`/articles`, {
+      method: `POST`,
+      data
+    });
+  }
+
+  async getCategories() {
+    return this._load(`/categories`);
+  }
+
+  getArticles() {
+    return this._load(`/articles`);
+  }
+
+  getArticle(id) {
+    return this._load(`/articles/${id}`);
+  }
+
+  search(query) {
+    return this._load(`/search`, { params: {query} });
+  }
 }
