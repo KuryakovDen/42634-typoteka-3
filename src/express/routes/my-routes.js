09d341a7e4d6d1329f85ac5c132856fc31fe88b3
Fifer2 <KuryakovDenis@yandex.ru>
2021-12-05 17:48:24 +0700

    feat: Получение данных для страницы my и myComments

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const api = require(`../api`).getAPI();
 const {Router} = require(`express`);
 const myRouter = new Router();
 
-myRouter.get(`/`, (req, res) => res.render(`my/my`));
-myRouter.get(`/comments`, (req, res) => res.render(`my/comments`));
+myRouter.get(`/`, async (req, res) => {
+  const myArticles = await api.getArticles();
+  res.render(`my`, {myArticles});
+});
+
+myRouter.get(`/comments`, async (req, res) => {
+  const articles = await api.getArticles();
+  res.render(`comments`, { articles: articles.slice(0, 3) })
+});
 
 module.exports = myRouter;
