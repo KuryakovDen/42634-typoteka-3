6d124c26e1d8ce3ba68e335f02baabd39d319db4
Fifer2 <KuryakovDenis@yandex.ru>
2021-12-05 18:19:03 +0700

    feat: Получение и вывод данных для страницы редактирования

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -2,10 +2,18 @@
 
 const {Router} = require(`express`);
 const articlesRouter = new Router();
+const api = require(`../api`).getAPI();
 
 articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles/articles-by-category`));
 articlesRouter.get(`/add`, (req, res) => res.render(`articles/post`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.render(`articles/post`));
-articlesRouter.get(`/:id`, (req, res) => res.render(`articles/post-detail`));
+
+articlesRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [article, categories] = await Promise.all([
+    api.getArticle(id),
+    api.getCategories()
+  ]);
+  res.render(`articles/post`, {article, categories});
+});
 
 module.exports = articlesRouter;
