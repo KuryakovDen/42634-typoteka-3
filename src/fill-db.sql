4553517aedb8b41fa3a6d4d27be4701b55c20195
dkuryakov <fifer2@yandex.ru>
2021-12-08 17:32:36 +0700

    feat: Написание запроса на заполнение таблицы users

---
+++ src/fill-db.sql
@@ -0,0 +1,8 @@
+// Запрос на заполнение таблицы users
+insert into users (name, lastname, email, password, avatar) values
+('Denis', 'Kuryakov', 'fifer2@yandex.ru', '123456', null),
+('Ivan', 'Ivanov', 'fifer16@yandex.ru', '5f4dcc3b5aa765d61d8327deb882cf99', 'avatar2.jpg'),
+('Petr', 'Petrov', 'petrov@yandex.ru', '5f4dcc3b5aa765d61d8327deb882cf98', 'avatar4.jpg'),
+('Vladimir', 'Vasin', 'vasin@mail.ru', '5f4dcc3b5aa765d61d8327deb882cf97', 'avatar1.jpg'),
+('Alyona', 'Gabisova', 'gadisova@mail.ru', '5f4dcc3b5aa765d61d8327deb882cf95', null),
+('Ekaterina', 'Baydasheva', 'baydasheva@yandex.ru', 'qwerty', null);
