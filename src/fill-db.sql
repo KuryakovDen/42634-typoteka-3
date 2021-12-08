c56536bd8d0b6816f425e02a48740547119b584b
dkuryakov <fifer2@yandex.ru>
2021-12-08 17:44:06 +0700

    feat: Написание запроса на заполнение таблицы categories

--- src/fill-db.sql
+++ src/fill-db.sql
@@ -1,8 +1,23 @@
 // Запрос на заполнение таблицы users
 insert into users (name, lastname, email, password, avatar) values
-('Denis', 'Kuryakov', 'fifer2@yandex.ru', '123456', null),
-('Ivan', 'Ivanov', 'fifer16@yandex.ru', '5f4dcc3b5aa765d61d8327deb882cf99', 'avatar2.jpg'),
-('Petr', 'Petrov', 'petrov@yandex.ru', '5f4dcc3b5aa765d61d8327deb882cf98', 'avatar4.jpg'),
-('Vladimir', 'Vasin', 'vasin@mail.ru', '5f4dcc3b5aa765d61d8327deb882cf97', 'avatar1.jpg'),
-('Alyona', 'Gabisova', 'gadisova@mail.ru', '5f4dcc3b5aa765d61d8327deb882cf95', null),
-('Ekaterina', 'Baydasheva', 'baydasheva@yandex.ru', 'qwerty', null);
+('Денис', 'Куряков', 'fifer2@yandex.ru', '123456', null),
+('Иван', 'Иванов', 'fifer16@yandex.ru', '5f4dcc3b5aa765d61d8327deb882cf99', 'avatar2.jpg'),
+('Петр', 'Петров', 'petrov@yandex.ru', '5f4dcc3b5aa765d61d8327deb882cf98', 'avatar4.jpg'),
+('Владимир', 'Васин', 'vasin@mail.ru', '5f4dcc3b5aa765d61d8327deb882cf97', 'avatar1.jpg'),
+('Алёна', 'Габисова', 'gadisova@mail.ru', '5f4dcc3b5aa765d61d8327deb882cf95', null),
+('Екатерина', 'Байдашева', 'baydasheva@yandex.ru', 'qwerty', null);
+
+// Запрос на заполнение таблицы categories
+insert into categories (name) values
+('Деревья'),
+('Разное'),
+('Автомобили'),
+('ИТ'),
+('За жизнь'),
+('Техника'),
+('Одежда'),
+('Музыка'),
+('Живопись'),
+('Аксессуары'),
+('Книги'),
+('Кино')
