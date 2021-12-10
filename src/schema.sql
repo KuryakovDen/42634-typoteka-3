f2c4ec35cdfdb8337dbda20ae75240d3f3fd9c91
dkuryakov <fifer2@yandex.ru>
2021-12-10 23:03:07 +0700

    refactor: Доработка запросов на создание таблиц

--- src/schema.sql
+++ src/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE public.users (
   id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name varchar(50) NOT NULL,
-  lastName varchar(80) NOT NULL,
+  last_name varchar(80) NOT NULL,
   email varchar(80) UNIQUE NOT NULL,
   password varchar(100) NOT NULL,
   avatar varchar(50)
@@ -15,38 +15,38 @@ CREATE TABLE public.categories (
 CREATE TABLE public.articles (
   id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   title varchar(150) NOT NULL,
-  createdDate timestamp DEFAULT current_timestamp,
+  created_date timestamp DEFAULT current_timestamp,
   image varchar(50) NOT NULL,
   announce varchar(250) NOT NULL,
-  fullText text NOT NULL,
-  userId integer NOT NULL,
-  FOREIGN KEY (userId) REFERENCES users(id)
+  full_text text NOT NULL,
+  user_id integer NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
 CREATE TABLE public.article_categories (
-  articleId integer NOT NULL,
-  categoryId integer NOT NULL,
-  PRIMARY KEY (articleId, categoryId),
-  FOREIGN KEY (articleId) REFERENCES articles(id)
+  article_id integer NOT NULL,
+  category_id integer NOT NULL,
+  PRIMARY KEY (article_id, category_id),
+  FOREIGN KEY (article_id) REFERENCES articles(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (categoryId) REFERENCES categories(id)
+  FOREIGN KEY (category_id) REFERENCES categories(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
 CREATE TABLE public.comments (
   id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  articleId integer NOT NULL,
-  userId integer NOT NULL,
-  createdDate timestamp NOT NULL,
+  article_id integer NOT NULL,
+  user_id integer NOT NULL,
+  created_date timestamp NOT NULL,
   text text NOT NULL,
-  FOREIGN KEY (articleId) REFERENCES articles(id)
+  FOREIGN KEY (article_id) REFERENCES articles(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  FOREIGN KEY (userId) REFERENCES users(id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
